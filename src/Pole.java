7f4fb23543d9edfa981c381f4428293836e321be
davidek <unluckycz@gmail.com>
2024-03-18 08:33:45 +0100

    zkouska3

---
+++ src/Pole.java
@@ -0,0 +1,12 @@
+public class Pole {
+
+    private int delka;
+
+    public int getDelka() {
+        return delka;
+    }
+
+    public void setDelka(int delka) {
+        this.delka = delka;
+    }
+}
