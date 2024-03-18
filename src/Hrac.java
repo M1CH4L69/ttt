26ac927f75aaf7005852c076d8a639a068255d53
davidek <unluckycz@gmail.com>
2024-03-18 08:45:27 +0100

    Vytvoreni tridy Hrac

---
+++ src/Hrac.java
@@ -0,0 +1,26 @@
+public class Hrac {
+
+    private String jmeno;
+    private char symbol;
+
+    public Hrac(String jmeno, char symbol) {
+        this.jmeno = jmeno;
+        this.symbol = symbol;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(char symbol) {
+        this.symbol = symbol;
+    }
+}
