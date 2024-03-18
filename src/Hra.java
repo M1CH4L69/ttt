c496dab3580671d60b53af9a55e8ba9bdb6e3d39
nemec6 <misanem06@gmail.com>
2024-03-18 08:41:15 +0100

    changes

--- src/Hra.java
+++ src/Hra.java
@@ -1,15 +1,3 @@
 public class Hra {
-    private String nazev;
 
-    public String getNazev() {
-        return nazev;
-    }
-
-    public void setNazev(String nazev) {
-        this.nazev = nazev;
-    }
-
-    public Hra(String nazev) {
-        this.nazev = nazev;
-    }
 }
