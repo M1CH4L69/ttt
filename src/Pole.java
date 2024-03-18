fe35457f4e6861721f3edd79cf965c83fc0e6947
davidek <unluckycz@gmail.com>
2024-03-18 08:42:46 +0100

    smazani

--- src/Pole.java
+++ src/Pole.java
@@ -1,16 +1,3 @@
 public class Pole {
 
-    private int delka;
-
-    public Pole(int delka) {
-        this.delka = delka;
-    }
-
-    public int getDelka() {
-        return delka;
-    }
-
-    public void setDelka(int delka) {
-        this.delka = delka;
-    }
 }
