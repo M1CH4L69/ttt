d1df9c30e547cee1ee477d1f3e2c3e5bf7c64d5c
nemec6 <misanem06@gmail.com>
2024-03-18 08:33:52 +0100

    Created Hra class

---
+++ src/Hra.java
@@ -0,0 +1,11 @@
+public class Hra {
+    private String nazev;
+
+    public String getNazev() {
+        return nazev;
+    }
+
+    public void setNazev(String nazev) {
+        this.nazev = nazev;
+    }
+}
