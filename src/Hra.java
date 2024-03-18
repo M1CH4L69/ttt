0aa19e253bc98889c8cac376b206ed6bd8b7aa97
davidek <unluckycz@gmail.com>
2024-03-18 08:59:27 +0100

    pridani symbolu

--- src/Hra.java
+++ src/Hra.java
@@ -2,4 +2,12 @@ public class Hra {
 
     private Hrac hrac1;
     private Hrac hrac2;
+
+    public void pridatSymbol(){
+        hrac1.setSymbol('o');
+        hrac2.setSymbol('x');
+    }
+
+
+
 }
