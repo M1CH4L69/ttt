a70eb1d0417fc056986ef7c237e8f21955bc39e5
nemec6 <misanem06@gmail.com>
2024-03-18 08:45:01 +0100

    something in Pole

--- src/Pole.java
+++ src/Pole.java
@@ -1,3 +1,25 @@
 public class Pole {
+    private int delka;
+    private int sirka;
 
+    public Pole(int delka, int sirka) {
+        this.delka = delka;
+        this.sirka = sirka;
+    }
+
+    public int getDelka() {
+        return delka;
+    }
+
+    public void setDelka(int delka) {
+        this.delka = delka;
+    }
+
+    public int getSirka() {
+        return sirka;
+    }
+
+    public void setSirka(int sirka) {
+        this.sirka = sirka;
+    }
 }
